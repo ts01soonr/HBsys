6d0dd0b90fc70e87e88b3010c12dba822d15eec2
Fang Zhiyong <fzhiyong@datto.com>
2024-05-27 13:30:25 +0200

    update port number

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ sudo cp asoonr.service /lib/systemd/system/asoonr.service
 sudo chmod 644 /lib/systemd/system/asoonr.service
 sudo systemctl daemon-reload
 sudo systemctl enable asoonr.service
+sleep 10
 sudo systemctl start asoonr.service
-
+sudo rm *.*
